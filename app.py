c41454be4a28042cab4f8915c0a20adf0031383c
Gar Cheuy <garcheuy@yahoo.com>
2018-02-16 15:15:29 -0800

    commit

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 # Import dependencies
 from flask import Flask, jsonify, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Session
 
 #################################################
 # Flask Setup
@@ -13,22 +17,40 @@ app = Flask(__name__)
 
 
 # Main route 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
 
 @app.route('/names')
 def names():
-    return jsonify()
+    # return jsonify()
 
-@app.route('/otu')
+    Base = declarative_base()
 
-@app.route('/metadata/<sample>')
+    class Samples(Base):
+        __tablename__ = "samples"
+        # otu_id = Column(Integer, primary_key=True)
 
-@app.route('/wfreq/<sample>')
+    # Create Database Connection
+    # ----------------------------------
+    # Establish Connection to database
+    engine = create_engine('sqlite:///DataSets/belly_button_biodiversity.sqlite')
+    Base.metadata.create_all(engine)
+    session = Session(bind=engine)
+    # Print all of the player names in the database
+    sampleNames = session.query(Samples.column_descriptions)# Samples.columns.keys().name
+    # What kind of object is `players`?
+    # print(f'\nSample name: {sampleNames}')
+    # Type of `players`: <class 'sqlalchemy.orm.query.Query'>
+    # http://docs.sqlalchemy.org/en/latest/orm/query.html
 
-@app.route('/samples/<sample>')
+# @app.route('/otu')
+
+# @app.route('/metadata/<sample>')
+
+# @app.route('/wfreq/<sample>')
+
+# @app.route('/samples/<sample>')
 
 if __name__ == "__main__":
     app.run(debug=True)
