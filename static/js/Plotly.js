fd755dfe55106bcb72d838bc2741f2defae76a1b
garc9394 <garcheuy@yahoo.com>
2018-02-21 01:07:45 -0800

    commit

--- static/js/Plotly.js
+++ static/js/Plotly.js
@@ -1,24 +1,35 @@
-function getNames() {
-    var url = `/names`;
+var response = ["BB_940", "BB_960"];
+var select = document.getElementById("selDataset");
 
-    d3.json(url, function(error, response) {
-
-        if (error) {
-            return console.warn(error);
-        }
-
-        // response = ["BB_940", "BB_960"]
-        var select = document.getElementById("selDataset");
-  
-        for (var i = 0; i < response.length; i++) {
-            var opt = response[i];
-            var ele = document.createElement("option");
-            ele.textContent = opt;
-            ele.value = opt;
-            select.appendChild(ele);
-        }
-    })
+for (var i = 0; i < response.length; i++) {
+    var opt = response[i];
+    var ele = document.createElement("option");
+    ele.textContent = opt;
+    ele.value = opt;
+    select.appendChild(ele);
 }
+
+// function getNames() {
+//     var url = `/names`;
+
+//     d3.json(url, function(error, response) {
+
+//         if (error) {
+//             return console.warn(error);
+//         }
+
+//         // response = ["BB_940", "BB_960"];
+//         var select = document.getElementById("selDataset");
+  
+//         for (var i = 0; i < response.length; i++) {
+//             var opt = response[i];
+//             var ele = document.createElement("option");
+//             ele.textContent = opt;
+//             ele.value = opt;
+//             select.appendChild(ele);
+//         }
+//     })
+// }
 
 function optionChanged() {
 
