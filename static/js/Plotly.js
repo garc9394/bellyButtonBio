a7dc1e76b124b14c73d39d1ca16951c3054cfd5c
garc9394 <garcheuy@yahoo.com>
2018-02-21 13:49:02 -0800

    commit

--- static/js/Plotly.js
+++ static/js/Plotly.js
@@ -1,12 +1,12 @@
 var response = ["BB_940", "BB_960"];
-var select = document.getElementById("selDataset");
 
 for (var i = 0; i < response.length; i++) {
-    var opt = response[i];
-    var ele = document.createElement("option");
-    ele.textContent = opt;
-    ele.value = opt;
-    select.appendChild(ele);
+    var optVal = response[i];
+    var option = document.createElement("option");
+    option.textContent = optVal;
+    option.value = optVal;
+    var select = document.getElementById("selDataset");
+    select.appendChild(option);
 }
 
 // function getNames() {
