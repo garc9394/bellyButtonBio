66d33a0c999c08761adea92ce714e1b09af28354
garc9394 <garcheuy@yahoo.com>
2018-03-28 00:18:31 -0700

    Commit

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# Import dependencies
 from flask import Flask, jsonify, render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
@@ -17,9 +16,6 @@ session = Session(engine)
 def index():
     return render_template('index.html')
 
-# sampleName = "BB_940"
-# sample = "940"
-
 @app.route("/names")
 def getNames():
     sampleNamesList = []
@@ -27,30 +23,22 @@ def getNames():
     for name in sampleNames:
         sampleNamesList.append('BB_' + str(name.SAMPLEID))
 
-    # sampleNamesList = ["BB_940", "BB_960"]
     return jsonify(sampleNamesList)
 
 @app.route('/otu')
 def otu():
     otuDescList = []
     otuDesc = session.execute("SELECT otu_id, lowest_taxonomic_unit_found FROM otu").fetchall()
-    class OTU(Base):
-        __tablename__ = "otu"
-        otu_id = Column(Integer, primary_key=True)
-        lowest_taxonomic_unit_found = Column(Text)
     for desc in otuDesc:
-        otuDescList.append(desc.otu_id)
         otuDescList.append(desc.lowest_taxonomic_unit_found)
 
     return jsonify(otuDescList)
 
 @app.route('/metadata/<sample>')
-# @app.route('/metadata/BB_940')
 def metadata(sample):
     sample = sample.replace("BB_", "")
     samplesData = {}
-    samplesData = session.execute("SELECT * FROM samples_metadata WHERE SAMPLEID = sample", {"sample":sample}).fetchall()
-    engine = create_engine('sqlite:///DataSets/belly_button_biodiversity.sqlite')
+    samplesMetadata = session.execute("SELECT * FROM samples_metadata WHERE SAMPLEID = :sample", {"sample":sample}).fetchall()
     for dataField in samplesMetadata:
         samplesData = {"AGE": dataField.AGE, 
                         "BBTYPE": dataField.BBTYPE, 
@@ -62,39 +50,31 @@ def metadata(sample):
     return jsonify(samplesData)
 
 @app.route('/wfreq/<sample>')
-# @app.route('/wfreq/BB_940')
 def wfreq(sample):
     sample = sample.replace("BB_", "")
-    engine = create_engine('sqlite:///DataSets/belly_button_biodiversity.sqlite')
-    Base.metadata.create_all(engine)
-    session = Session(bind=engine)
-    wfreqMetadata = session.query(SamplesMetadata).filter_by(SAMPLEID=sample)
-    for dataField in wfreqMetadata:
-        # wfreqData[sample].append('BB_' + str(name.SAMPLEID))
-        wfreqData = {"WFREQ": dataField.WFREQ}
-
-    return jsonify(wfreqData)
+    wfreqData = session.execute("SELECT WFREQ FROM samples_metadata WHERE SAMPLEID = :sample", {"sample":sample}).fetchall()
+    for wfreqField in wfreqData:
+        wfreq = str(wfreqField.WFREQ)
+    return jsonify(wfreq)
 
 @app.route('/samples/<sample>')
-# @app.route('/samples/BB_940')
 def samples(sample):
-    engine = create_engine('sqlite:///DataSets/belly_button_biodiversity.sqlite')
-    Base.metadata.create_all(engine)
-    session = Session(bind=engine)
-    sample_ids = session.query(Samples).filter(sample).order_by(desc('otu_id'))
+    sqlQuery = "SELECT otu_id, " + sample + " AS sample_value FROM samples WHERE " + sample + " > 0 ORDER BY " + sample + " DESC"
+    sample_ids = session.execute(sqlQuery).fetchall()
+
+    idsList = []
+    valuesList = []
     sampleIdsList = []
+    sampleDict = {}
     for sampleId in sample_ids:
-        sampleIdsList.append(sampleId.otu_id)
+        idsList.append(sampleId.otu_id)
+        valuesList.append(sampleId.sample_value)
 
-    sample_values = session.query(Samples).filter(sample).order_by(desc(sample))
-    sampleValuesList = []
-    for sampleValue in sample_values:
-        sampleValuesList.append(sampleValue.BB_940)
+    sampleDict = {"otu_ids": idsList, 
+                    "sample_values": valuesList}
+    sampleIdsList.append(sampleDict)
 
-    sampleData = {"otu_ids": sampleIdsList,
-                    "sample_values": sampleValuesList}
-    return jsonify(sampleData)
-
+    return jsonify(sampleIdsList)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/Plotly.js
+++ static/js/Plotly.js
@@ -1,57 +1,108 @@
-// var response = ["BB_940", "BB_960"];
+var namesUrl = "/names";
 
-// for (var i = 0; i < response.length; i++) {
-//     var optVal = response[i];
-//     var option = document.createElement("option");
-//     option.textContent = optVal;
-//     option.value = optVal;
-//     var select = document.getElementById("selDataset");
-//     select.appendChild(option);
-// }
+d3.json(namesUrl, function(error, samples) {
 
-// getNames();
+    if (error) {
+        return console.warn(error);
+    }
 
-// function getNames() {
-    var url = "/names";
+    var select = document.getElementById("selDataset");
 
-    // d3.json("/names", function(error, response) {
-    d3.json(url, function(error, response) {
+    for (var i = 0; i < samples.length; i++) {
+        var optVal = samples[i];
+        var option = document.createElement("option");
+        option.textContent = optVal;
+        option.value = optVal;
+        select.appendChild(option);
+    }
+});
 
-        if (error) {
-            return console.warn(error);
-        }
+function optionChanged(sample) {
 
-        // console.log(response);
-        // response = ["BB_940", "BB_960"];
-        var select = document.getElementById("selDataset");
-  
-        for (var i = 0; i < response.length; i++) {
-            var optVal = response[i];
-            var option = document.createElement("option");
-            option.textContent = optVal;
-            option.value = optVal;
-            select.appendChild(option);
-        }
-    });
-// }
-
-function optionChanged() {
-    // var sample = document.getElementById('selDataset'.value);
-    // console.log(sample)
-    var sampleValue = sample;
-    var sampleUrl = "/samples/";
-    // var url = "/samples/BB_940";
-
+    var sampleUrl = "/metadata/";
     d3.select('#sampleSummary').selectAll('li').remove();
+    d3.json(sampleUrl + sample, function(error, samplesData) {
 
-    // d3.json(url, function(error, response) {
+        d3.select('#sampleSummary')
+        .append('li').attr('id', 'age').text('AGE: ' + samplesData["AGE"])
+        .append('li').attr('id', 'bbtype').text('BBTYPE: ' + samplesData["BBTYPE"])
+        .append('li').attr('id', 'ethnicity').text('ETHNICITY: ' + samplesData["ETHNICITY"])
+        .append('li').attr('id', 'gender').text('GENDER: ' + samplesData["GENDER"])
+        .append('li').attr('id', 'location').text('LOCATION: ' + samplesData["LOCATION"])
+        .append('li').attr('id', 'sampleid').text('SAMPLEID: ' + samplesData["SAMPLEID"])
 
-    //     if (error) {
-    //         return console.warn(error);
-    //     }
+    });
 
-    //     for (var i = 0; i < response.length; i++) {
-    //         console.log(response[i]);
-    //     }
+    // var sampleValueUrl = "/samples/";
+    var sampleValues = [19, 26, 55];
+    // d3.json(sampleValueUrl + sample, function(error, sampleIdsList) {
+    //     // for (i = 0; i < 10; i++) { 
+    //         sampleValues.push(sampleIdsList["sample_value"]);
+    //     // }
+    // });
+
+    // var namesUrl = "/otu";
+    // d3.json(namesUrl, function(error, otuDescList) {
+    //     console.log(otuDescList);
+    // });
+
+    var labelValues = ['Residential', 'Non-Residential', 'Utility'];
+
+    var data = [{
+        values: sampleValues,
+        labels: labelValues,
+        type: 'pie'
+    }];
+    
+    var layout = {
+    height: 350,
+    width: 400
+    };
+    
+    Plotly.newPlot('pieChart', data, layout);
+
+    // var sampleValueUrl = "/samples/"
+
+    // // d3.select('#sampleValues').selectAll('li').remove();
+    // d3.select('#pieChart').selectAll('li').remove();
+
+    // d3.json(sampleValueUrl + sample, function(error, sampleIdsList) {
+
+    //     console.log(sampleIdsList);
+    //     // d3.select('#sampleValues')
+    //     // d3.select('#pieChart')
+    //     // .append('li').attr('id', '1st_value').text(sampleIdsList[1])
+
+    // });
+
+
+
+    /////////////////////////////////////
+    // for SQL query verification only //
+    /////////////////////////////////////
+
+    // var namesUrl = "/names";
+    // d3.json(namesUrl, function(error, samples) {
+    //     console.log(samples);
+    // });
+
+    // var namesUrl = "/otu";
+    // d3.json(namesUrl, function(error, otuDescList) {
+    //     console.log(otuDescList);
+    // });
+
+    // var sampleUrl = "/metadata/";
+    // d3.json(sampleUrl + sample, function(error, samplesData) {
+    //     console.log(samplesData);
+    // });
+
+    // var wfreqUrl = "/wfreq/";
+    // d3.json(wfreqUrl + sample, function(error, wfreq) {
+    //     console.log(wfreq);
+    // });
+
+    // var sampleValueUrl = "/samples/";
+    // d3.json(sampleValueUrl + sample, function(error, sampleIdsList) {
+    //     console.log(sampleIdsList);
     // });
 }
